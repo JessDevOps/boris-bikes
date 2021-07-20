4567c43a1bfb62e6550c61a8741b230290598aba
Jessica <Jessica_doyle@outlook.com>
2021-07-20 15:09:40 +0100

    Up to 12 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
 require_relative 'bike' # without this need 'require ./lib/bike.rb' to run
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail 'no bikes available' unless @bike
+    @bike
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
+
+  
